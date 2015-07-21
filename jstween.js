d715adaa200715bc44c00926de001610bdb3e917
shrek.wang <shrekshrek@gmail.com>
2015-07-22 02:51:47 +0800

    update

--- jstween.js
+++ jstween.js
@@ -15,9 +15,6 @@
     });
     if ("now" in window.performance == false){
         var nowOffset = Date.now();
-        if (performance.timing && performance.timing.navigationStart){
-            nowOffset = performance.timing.navigationStart
-        }
         window.performance.now = function now(){
             return Date.now() - nowOffset;
         }
